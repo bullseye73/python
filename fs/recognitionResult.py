8215341c2b07d04c5a45b5da9d53c2485a29c1f0
bullseye <bullseye73@gmail.com>
2019-06-11 11:24:19 +0900

    multi file read

--- fs/recognitionResult.py
+++ fs/recognitionResult.py
@@ -1,30 +1,30 @@
 import os
 import xlsxwriter
 
-def readFileName(path):
-	workbook = xlsxwriter.Workbook('readresult_01.xlsx')
-	worksheet = workbook.add_worksheet()
+def readFileName(ws, path):
 	filenames = os.listdir(path)
 
-	worksheet.write('A1', 'Path')
-	worksheet.write('B1', 'FileName')
-
+	ws.write('A1', 'Path')
+	ws.write('B1', 'FileName')
 	row = 1
 	col = 0
+
 	for filename in filenames:
-		worksheet.write(row, col, path)
-		worksheet.write(row, col+1, filename)
+		ws.write(row, col, path)
+		ws.write(row, col+1, filename)
 		row += 1
 		#full_filename = os.path.join(path, filename)
 		#print (full_filename)
 
-	workbook.close()
-
 def main():
 	for i in os.listdir("./"):
 		if os.path.isdir(i):
-			print(i)
-			readFileName(i)
+			rf = i + '.xlsx'
+			print(rf)
+			workbook = xlsxwriter.Workbook(rf)
+			worksheet = workbook.add_worksheet()
+			readFileName(worksheet, i)
+			workbook.close()
 
 
 if __name__ == '__main__':
