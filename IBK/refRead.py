d6d0d7f00e27a4312b9f4c0e9ab9f5eff479555a
bullseye73 <bullseye73@gmail.com>
2019-08-14 12:10:02 +0900

    개별 인식 결과 txt 리스트를 읽어서 통합 txt를 생성하는 프로그램

--- IBK/refRead.py
+++ IBK/refRead.py
@@ -4,62 +4,26 @@ import os, codecs
 import openpyxl
 import sys, re, time
 
-def makeFileName (str):
-    fn = re.sub('[-=.#/?:$}]', '', str)
+def makeFileName ():
+    fn = "result"
     now = time.localtime()
     s = "%04d%02d%02d_%02d%02d%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-    return fn + "_" + s + ".xlsx"
+    return fn + "_" + s + ".txt"
 
 def readTxtFile_ex(fn):
-    fi = os.path.split(fn)
-    fname, ext = os.path.splitext(fi[1])
+    file_list = os.listdir(fn)
+    retfilename = fn + "\\" + makeFileName()
+    wf = open(retfilename, mode='wt', encoding='utf-8')
 
-    workbook = openpyxl.Workbook()
-    worksheet = workbook.active
-    worksheet.title = "Ref"
-
-    with codecs.open(fn, 'r', encoding="utf-8-sig") as f:
-        #titles = ["FileName","Ref.NO.", "Data"]
-        xlData = list()
-        isReadRef = False
-        isStart = True
-
-        #worksheet.append(titles)
-        for line in f:
-            r = line.replace('\r\n', '').strip()
-
-            if "jpg" in r.lower():
-                if isStart:
-                    xlData.append(r)
-                    isStart = False
-                else:
-                    worksheet.append(xlData)
-                    xlData.clear()
-                    xlData.append(r)
-                    isReadRef = False
-                    isStart = True
-            elif "ref" in r.lower():
-                ref = r.split(" ", 1 )
-                if len(ref) <= 1:
-                    isReadRef = True
-                    xlData.append(r)
-                else:
-                    xlData.append(ref[0])
-                    xlData.append(ref[1])
-                    worksheet.append(xlData)
-                    xlData.clear()
-                    isReadRef = False
-                    isStart = True
-            else:
-                if isReadRef:
-                    xlData.append(r)
-                    worksheet.append(xlData)
-                    xlData.clear()
-                    isReadRef = False
-                    isStart = True
-
-    workbook.save(makeFileName(fname))
-    workbook.close()
+    for f in file_list:
+        if f.endswith(".txt"):
+            print(f)
+            with codecs.open(fn+"\\"+f, 'r', encoding="utf-8-sig") as rf:
+                wf.write('"set name","' + f + '"\n')
+                wf.write('"file name","' + f + '"\n')
+                wf.write(rf.read())
+    wf.close()
+    #with codecs.open(fn, 'r', encoding="utf-8-sig") as f:
 
 
 def Usage():
